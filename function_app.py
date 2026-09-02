c17d00eb641e868ed3832253700ea37edcf7052f
shinoacute <167141772+shinoacute@users.noreply.github.com>
2026-09-01 19:51:03 -0700

    Implement CORS and user authentication in function app

--- function_app.py
+++ function_app.py
@@ -1,7 +1,84 @@
+import json
+import os
+
 import azure.functions as func
+import pyodbc
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+app = func.FunctionApp()
 
-@app.route(route="hello")
-def hello(req):
-  return func.HttpResponse("Hello")
+ALLOWED_ORIGINS = {
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "http://127.0.0.1:5000",
+    "http://localhost:5000",
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+}
+
+CONNECTION_STRING = (
+    "DRIVER={SQL Server};"
+    f"SERVER={os.environ['SQL_SERVER']};"
+    f"DATABASE={os.environ['SQL_DATABASE']};"
+    f"UID={os.environ['SQL_USERNAME']};"
+    f"PWD={os.environ['SQL_PASSWORD']};"
+    "TrustServerCertificate=no;"
+    "Connection Timeout=30;"
+)
+
+
+def add_cors_headers(req: func.HttpRequest, response: func.HttpResponse) -> func.HttpResponse:
+    origin = req.headers.get("Origin")
+    if origin in ALLOWED_ORIGINS:
+        response.headers["Access-Control-Allow-Origin"] = origin
+        response.headers["Vary"] = "Origin"
+    return response
+
+
+def check_credentials(username, password):
+    with pyodbc.connect(CONNECTION_STRING) as connection:
+        cursor = connection.cursor()
+        cursor.execute(
+            """
+            SELECT 1
+            FROM dbo.[user]
+            WHERE username = ? AND password = ?
+            """,
+            username,
+            password,
+        )
+        return cursor.fetchone() is not None
+
+
+@app.route(route="", methods=["GET"], auth_level=func.AuthLevel.ANONYMOUS)
+def index(req: func.HttpRequest) -> func.HttpResponse:
+    body = {
+        "message": "Login successful, welcome!",
+        "username": req.params.get("username"),
+    }
+    response = func.HttpResponse(json.dumps(body), mimetype="application/json")
+    return add_cors_headers(req, response)
+
+
+@app.route(route="login", methods=["GET"], auth_level=func.AuthLevel.ANONYMOUS)
+def login(req: func.HttpRequest) -> func.HttpResponse:
+    username = req.params.get("username")
+    password = req.params.get("password")
+
+    if not username or not password:
+        response = func.HttpResponse(
+            json.dumps({"error": "username and password are required"}),
+            status_code=400,
+            mimetype="application/json",
+        )
+        return add_cors_headers(req, response)
+
+    if check_credentials(username, password):
+        response = func.HttpResponse(
+            json.dumps({"authenticated": True}), status_code=200, mimetype="application/json"
+        )
+    else:
+        response = func.HttpResponse(
+            json.dumps({"authenticated": False}), status_code=401, mimetype="application/json"
+        )
+
+    return add_cors_headers(req, response)
