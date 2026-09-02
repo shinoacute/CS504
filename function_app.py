26bdfe0de403a5bbe8d2e3c1985430732f62dca8
shinoacute <167141772+shinoacute@users.noreply.github.com>
2026-09-01 20:17:19 -0700

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -1,84 +1,53 @@
-import json
 import os
+import unittest
 
-import azure.functions as func
-import pyodbc
-
-app = func.FunctionApp()
-
-ALLOWED_ORIGINS = {
-    "http://127.0.0.1:5500",
-    "http://localhost:5500",
-    "http://127.0.0.1:5000",
-    "http://localhost:5000",
-    "http://127.0.0.1:3000",
-    "http://localhost:3000",
-}
-
-CONNECTION_STRING = (
-    "DRIVER={SQL Server};"
-    "SERVER=cityuweek8thao.database.windows.net;"
-    "DATABASE=week8-thao;"
-    "UID=thao-admin;"
-    "PWD=Buihoangco123.;"
-    "TrustServerCertificate=no;"
-    "Connection Timeout=30;"
-)
+import function_app
 
 
-def add_cors_headers(req: func.HttpRequest, response: func.HttpResponse) -> func.HttpResponse:
-    origin = req.headers.get("Origin")
-    if origin in ALLOWED_ORIGINS:
-        response.headers["Access-Control-Allow-Origin"] = origin
-        response.headers["Vary"] = "Origin"
-    return response
+class FunctionAppTests(unittest.TestCase):
+    def setUp(self):
+        self.original_env = {
+            name: os.environ.get(name)
+            for name in [
+                "AZURE_SQL_SERVER",
+                "AZURE_SQL_DATABASE",
+                "AZURE_SQL_USERNAME",
+                "AZURE_SQL_PASSWORD",
+            ]
+        }
+
+    def tearDown(self):
+        for key, value in self.original_env.items():
+            if value is None:
+                os.environ.pop(key, None)
+            else:
+                os.environ[key] = value
+
+    def test_get_connection_string_sets_values_from_environment(self):
+        os.environ["AZURE_SQL_SERVER"] = "cityuweek8thao.database.windows.net"
+        os.environ["AZURE_SQL_DATABASE"] = "week8-thao"
+        os.environ["AZURE_SQL_USERNAME"] = "thao-admin"
+        os.environ["AZURE_SQL_PASSWORD"] = "Buihoangco123."
+
+        connection_string = function_app.get_connection_string()
+
+        self.assertIn("SERVER=server.database.windows.net", connection_string)
+        self.assertIn("DATABASE=appdb", connection_string)
+        self.assertIn("UID=appuser", connection_string)
+        self.assertIn("PWD=secret", connection_string)
+
+    def test_get_connection_string_requires_all_values(self):
+        for key in [
+            "AZURE_SQL_SERVER",
+            "AZURE_SQL_DATABASE",
+            "AZURE_SQL_USERNAME",
+            "AZURE_SQL_PASSWORD",
+        ]:
+            os.environ.pop(key, None)
+
+        with self.assertRaises(RuntimeError):
+            function_app.get_connection_string()
 
 
-def check_credentials(username, password):
-    with pyodbc.connect(CONNECTION_STRING) as connection:
-        cursor = connection.cursor()
-        cursor.execute(
-            """
-            SELECT 1
-            FROM dbo.[user]
-            WHERE username = ? AND password = ?
-            """,
-            username,
-            password,
-        )
-        return cursor.fetchone() is not None
-
-
-@app.route(route="", methods=["GET"], auth_level=func.AuthLevel.ANONYMOUS)
-def index(req: func.HttpRequest) -> func.HttpResponse:
-    body = {
-        "message": "Login successful, welcome!",
-        "username": req.params.get("username"),
-    }
-    response = func.HttpResponse(json.dumps(body), mimetype="application/json")
-    return add_cors_headers(req, response)
-
-
-@app.route(route="login", methods=["GET"], auth_level=func.AuthLevel.ANONYMOUS)
-def login(req: func.HttpRequest) -> func.HttpResponse:
-    username = req.params.get("username")
-    password = req.params.get("password")
-
-    if not username or not password:
-        response = func.HttpResponse(
-            json.dumps({"error": "username and password are required"}),
-            status_code=400,
-            mimetype="application/json",
-        )
-        return add_cors_headers(req, response)
-
-    if check_credentials(username, password):
-        response = func.HttpResponse(
-            json.dumps({"authenticated": True}), status_code=200, mimetype="application/json"
-        )
-    else:
-        response = func.HttpResponse(
-            json.dumps({"authenticated": False}), status_code=401, mimetype="application/json"
-        )
-
-    return add_cors_headers(req, response)
+if __name__ == "__main__":
+    unittest.main()
